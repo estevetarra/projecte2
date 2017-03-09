a1f874e347d9a12af70af6c421fd2b48ac573697
estevetarra <ets1995@hotmail.com>
2017-03-09 19:46:51 +0100

    Update installation.sh
    deleted nom

--- documentacio/installation.sh
+++ documentacio/installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash - 
 echo -e "$RED Debian or Debian derivative OS distro detected, installing programs.. $STD"
-DEBIAN_PROGS="nodejs npm mongodb build-essential libopencv-dev"
+DEBIAN_PROGS="nodejs mongodb build-essential libopencv-dev"
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y $DEBIAN_PROGS
