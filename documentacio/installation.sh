470d0db3e25c6972f4bd3a452f343f020daee8dc
estevetarra <ets1995@hotmail.com>
2017-03-09 19:32:16 +0100

    Create installation.sh
    1. Installing nodejs npm mongodb build-essential libopencv-dev
    2. Downloading the repo
    3. Installing the repo dependencies with npm

---
+++ documentacio/installation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash - 
+echo -e "$RED Debian or Debian derivative OS distro detected, installing programs.. $STD"
+DEBIAN_PROGS="nodejs npm mongodb build-essential libopencv-dev"
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y $DEBIAN_PROGS
+git clone https://github.com/estevetarra/projecte2.git
+cd projecte2
+cd codi
+nmp install
