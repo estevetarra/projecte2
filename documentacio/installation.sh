6d43f4e6ca5306f8044106ad8429999823bbddeb
estevetarra <ets1995@hotmail.com>
2017-03-10 12:35:00 +0100

    Update installation.sh

--- documentacio/installation.sh
+++ documentacio/installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash - 
 echo -e "$RED Debian or Debian derivative OS distro detected, installing programs.. $STD"
-DEBIAN_PROGS="nodejs mongodb build-essential libopencv-dev"
+DEBIAN_PROGS="nodejs  build-essential"
+#mongodb libopencv-dev"
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y $DEBIAN_PROGS
