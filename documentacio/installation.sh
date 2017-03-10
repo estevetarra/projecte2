ece8257e7442042db978abf727b426fb53934f8c
estevetarra <ets1995@hotmail.com>
2017-03-10 12:09:55 +0100

    Update installation.sh

--- documentacio/installation.sh
+++ documentacio/installation.sh
@@ -4,7 +4,7 @@ DEBIAN_PROGS="nodejs mongodb build-essential libopencv-dev"
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y $DEBIAN_PROGS
-git clone https://github.com/estevetarra/projecte2.git
-cd projecte2
-cd codi
-npm install
+#git clone https://github.com/estevetarra/projecte2.git
+#cd projecte2
+#cd codi
+#npm install
