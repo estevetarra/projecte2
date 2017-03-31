1ade42969516b71a650bfbb3110870bf7994a54d
Iñigo Moreno <ignigomoreno@gmail.com>
2017-03-31 13:05:22 +0200

    Changed button movement.

--- codi/app.js
+++ codi/app.js
@@ -31,18 +31,19 @@ app.get('/', function (req, res) {
 })
 
 app.post('/openDoor', function (req, res) {
-  if( servo_pos < 90){
-    //for (servo_pos = 0; servo_pos < 180; servo_pos+=10){
-      servo_pos=160;
-    
-  } else {
-    //for (servo_pos = 180; servo_pos > 0; servo_pos-=10){
-      servo_pos=20;   
+  function move_pos(pos){
+      var servo_pwm=servo_pos*0.2/180;
+      piblaster.setPwm(21,servo_pwm);
+      console.log ("pwm",servo_pwm);
+      console.log ("posicio",servo_pos);
   }
-  var servo_pwm=servo_pos*0.2/180;
-  piblaster.setPwm(21,servo_pwm);
-  console.log(servo_pwm);
-  console.log ("moviment finalitzat",servo_pos);
+
+
+  var servo_pwm=[0.088,0.05];
+  piblaster.setPwm(21,servo_pwm[0]);
+  setTimeout(function(){
+      piblaster.setPwm(21,servo_pwm[1]);
+  });
   var ret = {};
   ret.status = 0;
   res.json(ret);
